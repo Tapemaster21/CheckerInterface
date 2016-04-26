39a09b3c33a5a0f591e5216661c357de7c71347f
ryschro <spug911@gmail.com>
2016-04-25 23:31:22 -0500

    We did it possible things to add -stepping through log -make a better AI -make more gamecom's -add test boards

--- CheckerInterface/Interface.Designer.cs
+++ CheckerInterface/Interface.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Interface));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.check1 = new System.Windows.Forms.CheckBox();
@@ -110,6 +111,7 @@
             this.spot47 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.spot27 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -324,6 +326,7 @@
             this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.button1);
             this.groupBox3.Controls.Add(this.label3);
             this.groupBox3.Controls.Add(this.comboBox1);
             this.groupBox3.Controls.Add(this.buttonReset);
@@ -361,6 +364,7 @@
             // 
             // buttonReset
             // 
+            this.buttonReset.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.buttonReset.Location = new System.Drawing.Point(88, 19);
             this.buttonReset.Name = "buttonReset";
             this.buttonReset.Size = new System.Drawing.Size(75, 23);
@@ -1151,6 +1155,17 @@
             this.spot27.TabStop = false;
             this.spot27.Click += new System.EventHandler(this.spot27_Click);
             // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(90, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Show Log";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Interface
             // 
             this.AcceptButton = this.buttonStart;
@@ -1226,6 +1241,7 @@
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.spot27);
             this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximumSize = new System.Drawing.Size(1500, 508);
             this.MinimumSize = new System.Drawing.Size(665, 508);
             this.Name = "Interface";
@@ -1388,6 +1404,7 @@
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.CheckBox check1;
         private System.Windows.Forms.CheckBox check2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
